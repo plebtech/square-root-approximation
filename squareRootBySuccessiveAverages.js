e1669804ba31340be0a5a90d80fc6e435a4e3194
plebtech <nbmc@protonmail.com>
2020-02-20 19:17:38 -0600

    first commit.

---
+++ squareRootBySuccessiveAverages.js
@@ -0,0 +1,13 @@
+// applying Heron of Alexandria's formula for approximating a square root through successive averaging.
+// stops at four trailing decimal figures (via toFixed(4), i.e. when two successive guesses are within thousandths of each other).
+approximateRoot =  square => {
+    let guess = Math.random() * square;
+    let lastGuess = 0;
+    while (guess.toFixed(4) != lastGuess.toFixed(4)) {
+        console.log(guess);
+        lastGuess = guess;
+        guess = (guess + (square / guess)) / 2;
+    }
+    console.log(guess);
+}
+approximateRoot(4);
