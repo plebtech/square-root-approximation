8c9e8750f00b00d8dd33377a38cce4fea4163e84
plebtech <nbmc@protonmail.com>
2020-03-17 12:26:06 -0500

    changed function to const.

--- squareRootBySuccessiveAverages.js
+++ squareRootBySuccessiveAverages.js
@@ -1,6 +1,6 @@
 // applying Heron of Alexandria's formula for approximating a square root through successive averaging.
 // stops at four trailing decimal figures (via toFixed(4)), i.e. when two successive guesses are within thousandths of each other.
-approximateRoot = square => {
+const approximateRoot = square => {
     let guess = Math.random() * square; // begin from a random point between 0 and the square.
     let lastGuess = 0;
     while (guess.toFixed(4) != lastGuess.toFixed(4)) {
